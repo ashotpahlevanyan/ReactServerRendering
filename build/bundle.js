a653da1e6e80d7c5d8277f5f9a8be357d9d10f63
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2018-04-18 03:14:52 +0400

    added req.path as location props to StaticBrowser to be aware of the current location user tries to reach

--- build/bundle.js
+++ build/bundle.js
@@ -91,7 +91,7 @@ var app = (0, _express2.default)();
 app.use(_express2.default.static('public'));
 
 app.get('/', function (req, res) {
-	res.send((0, _renderer2.default)());
+	res.send((0, _renderer2.default)(req));
 });
 
 app.listen(3000, function () {
@@ -129,10 +129,10 @@ var _Routes2 = _interopRequireDefault(_Routes);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-exports.default = function () {
+exports.default = function (req) {
 	var content = (0, _server.renderToString)(_react2.default.createElement(
 		_reactRouterDom.StaticRouter,
-		{ context: {} },
+		{ location: req.path, context: {} },
 		_react2.default.createElement(_Routes2.default, null)
 	));
 
