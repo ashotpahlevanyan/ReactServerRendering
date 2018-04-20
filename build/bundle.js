5d5fae2b8890452c27ee4ae8d444a453f89ec5a0
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2018-04-21 01:09:25 +0400

    the third solution, which waits till every single request is done and then only renders the page, but we still do not show the user what is going on, and why we don't show the list of admins

--- build/bundle.js
+++ build/bundle.js
@@ -309,9 +309,15 @@ app.get('*', function (req, res) {
 		var route = _ref.route;
 
 		return route.loadData ? route.loadData(store) : null;
+	}).map(function (promise) {
+		if (promise) {
+			return new Promise(function (resolve, reject) {
+				promise.then(resolve).catch(resolve);
+			});
+		}
 	});
 
-	var render = function render() {
+	Promise.all(promises).then(function () {
 		var context = {};
 		var content = (0, _renderer2.default)(req, store, context);
 
@@ -320,9 +326,7 @@ app.get('*', function (req, res) {
 		}
 
 		res.send(content);
-	};
-
-	Promise.all(promises).then(render).catch(render);
+	});
 });
 
 app.listen(3000, function () {
